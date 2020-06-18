c232e1f61d188243b853eaa07fbf93e869a48061
Katrina Campbell <ktrncampbell@gmail.com>
2020-06-18 10:37:25 -0400

    inital commit

---
+++ app/src/main/java/com/kjc/quizme/view/QuizActivity.java
@@ -0,0 +1,219 @@
+package com.kjc.quizme.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.kjc.quizme.R;
+import com.kjc.quizme.database.QuizDbHelper;
+import com.kjc.quizme.model.Question;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class QuizActivity extends AppCompatActivity {
+
+    public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
+
+    private TextView questionTextView;
+    private TextView scoreTextView;
+    private TextView questionCountTextView;
+    private TextView counterTextView;
+    private RadioGroup rgRadioGroup;
+    private RadioButton rb1RadioButton;
+    private RadioButton rb2RadioButton;
+    private RadioButton rb3RadioButton;
+    private Button confirmNextButton;
+
+    private ColorStateList textColorDefaultRb;
+    private ColorStateList textColorDefaultCd;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
+    private List<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
+
+    private long backPressedTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        questionTextView = findViewById(R.id.question_textView);
+        scoreTextView = findViewById(R.id.score_textview);
+        questionCountTextView = findViewById(R.id.question_label_textview);
+        counterTextView = findViewById(R.id.counter_textview);
+        rgRadioGroup = findViewById(R.id.radio_group);
+        rb1RadioButton = findViewById(R.id.radio_button1);
+        rb2RadioButton = findViewById(R.id.radio_button2);
+        rb3RadioButton = findViewById(R.id.radio_button3);
+        confirmNextButton = findViewById(R.id.confirm_next_button);
+
+        textColorDefaultRb = rb1RadioButton.getTextColors();
+        textColorDefaultCd = counterTextView.getTextColors();
+
+        QuizDbHelper dbHelper = new QuizDbHelper(this);
+        questionList = dbHelper.getAllQuestions();
+        questionCountTotal = questionList.size();
+        Collections.shuffle(questionList);
+
+        showNextQuestion();
+
+        confirmNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!answered){
+                    if (rb1RadioButton.isChecked() || rb2RadioButton.isChecked() || rb3RadioButton.isChecked()){
+                        checkAnswer();
+                    }else{
+                        Toast.makeText(QuizActivity.this, "Please select an answer.", Toast.LENGTH_SHORT).show();
+                        showNextQuestion();
+                    }
+                }
+            }
+        });
+    }
+
+    private void showNextQuestion() {
+        rb1RadioButton.setTextColor(textColorDefaultRb);
+        rb2RadioButton.setTextColor(textColorDefaultRb);
+        rb3RadioButton.setTextColor(textColorDefaultRb);
+        rgRadioGroup.clearCheck();
+
+        if (questionCounter < questionCountTotal){
+            currentQuestion = questionList.get(questionCounter);
+
+            questionCountTextView.setText(currentQuestion.getQuestion());
+            rb1RadioButton.setText(currentQuestion.getOption1());
+            rb2RadioButton.setText(currentQuestion.getOption2());
+            rb3RadioButton.setText(currentQuestion.getOption3());
+
+            questionCounter++;
+            questionCountTextView.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false;
+            confirmNextButton.setText("Confirm");
+
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
+        } else{
+            finishQuiz();
+        }
+    }
+
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                checkAnswer();
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) ((timeLeftInMillis / 1000) / 60);
+        int seconds = (int) ((timeLeftInMillis / 1000) % 60);
+
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        counterTextView.setText(timeFormatted);
+
+        if (timeLeftInMillis < 10000){
+            counterTextView.setTextColor(Color.RED);
+        }else{
+            counterTextView.setTextColor(textColorDefaultCd);
+        }
+    }
+
+    private void checkAnswer() {
+        answered =true;
+
+        countDownTimer.cancel();
+
+        RadioButton rbSelected = findViewById(rgRadioGroup.getCheckedRadioButtonId());
+        int answerNmr = rgRadioGroup.indexOfChild(rbSelected) + 1;
+
+        if (answerNmr == currentQuestion.getAnswerNmr()){
+            score++;
+            scoreTextView.setText("Score: " + score);
+        }
+        showSolution();
+    }
+
+    private void showSolution() {
+        rb1RadioButton.setTextColor(Color.RED);
+        rb2RadioButton.setTextColor(Color.RED);
+        rb3RadioButton.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNmr()) {
+            case 1:
+                rb1RadioButton.setTextColor(Color.GREEN);
+                questionTextView.setText("Answer 1 is correct");
+                break;
+            case 2:
+                rb2RadioButton.setTextColor(Color.GREEN);
+                questionTextView.setText("Answer 2 is correct");
+                break;
+            case 3:
+                rb3RadioButton.setTextColor(Color.GREEN);
+                questionTextView.setText("Answer 3 is correct");
+                break;
+        }
+
+        if (questionCounter < questionCountTotal){
+            confirmNextButton.setText("Next");
+        } else {
+            confirmNextButton.setText("Finish");
+        }
+    }
+
+    private void finishQuiz() {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()){
+            finishQuiz();
+        }else{
+            Toast.makeText(this, "Press back again to finish.", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null){
+            countDownTimer.cancel();
+        }
+    }
+}
